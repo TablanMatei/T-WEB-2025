f63f4489ac927098dc47fc6b23ff64ac05299edb
Tablan Matei-Christian <matei.tablan@gmail.com>
2025-06-20 01:32:05 +0300

    Frontend - curenttlyReading & wantToRead

--- frontend/wantToReadPage/wantToReadPage.js
+++ frontend/wantToReadPage/wantToReadPage.js
@@ -497,7 +497,7 @@ function addDashboardDropdown() {
             </a>
           </li>
           <li>
-            <a href="#want-to-read">
+            <a href="wantToReadPage.html">
               <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24" fill="none" stroke="#7a4e3e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M12 20l9-5-9-5-9 5 9 5z"></path>
                 <path d="M12 12v8"></path>
